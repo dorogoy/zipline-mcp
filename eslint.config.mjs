9ca9c232c1362fd0d96305aa97131a3fbab380c3
Sergio Rael <s.rael@valutico.com>
2025-08-10 19:09:09 +0200

    build(config): switch eslint to prettier-config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,51 +1,53 @@
-import typescriptEslint from "@typescript-eslint/eslint-plugin";
-import globals from "globals";
-import tsParser from "@typescript-eslint/parser";
-import path from "node:path";
-import { fileURLToPath } from "node:url";
-import js from "@eslint/js";
-import { FlatCompat } from "@eslint/eslintrc";
+import typescriptEslint from '@typescript-eslint/eslint-plugin';
+import globals from 'globals';
+import tsParser from '@typescript-eslint/parser';
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
+import js from '@eslint/js';
+import { FlatCompat } from '@eslint/eslintrc';
+import prettierConfig from 'eslint-config-prettier';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const compat = new FlatCompat({
-    baseDirectory: __dirname,
-    recommendedConfig: js.configs.recommended,
-    allConfig: js.configs.all
+  baseDirectory: __dirname,
+  recommendedConfig: js.configs.recommended,
+  allConfig: js.configs.all,
 });
 
 export default [
-    { ignores: ["**/dist/**", "**/node_modules/**"] },
-    ...compat.extends(
-        "eslint:recommended",
-        "plugin:@typescript-eslint/recommended",
-        "plugin:@typescript-eslint/recommended-requiring-type-checking",
-        "prettier",
-    ),
-    {
-        plugins: {
-            "@typescript-eslint": typescriptEslint,
-        },
-
-        languageOptions: {
-            globals: {
-                ...globals.node,
-            },
-
-            parser: tsParser,
-            ecmaVersion: 2022,
-            sourceType: "module",
-
-            parserOptions: {
-                project: "./tsconfig.json",
-            },
-        },
-
-        rules: {
-            semi: ["error", "always"],
-            "no-unused-vars": "off",
-            "@typescript-eslint/no-unused-vars": ["warn"],
-            "@typescript-eslint/explicit-function-return-type": "off",
-        },
+  { ignores: ['**/dist/**', '**/node_modules/**'] },
+  ...compat.extends(
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended-requiring-type-checking'
+  ),
+  // Disable rules that might conflict with Prettier formatting
+  prettierConfig,
+  {
+    plugins: {
+      '@typescript-eslint': typescriptEslint,
     },
+
+    languageOptions: {
+      globals: {
+        ...globals.node,
+      },
+
+      parser: tsParser,
+      ecmaVersion: 2022,
+      sourceType: 'module',
+
+      parserOptions: {
+        project: './tsconfig.json',
+      },
+    },
+
+    rules: {
+      semi: ['error', 'always'],
+      'no-unused-vars': 'off',
+      '@typescript-eslint/no-unused-vars': ['warn'],
+      '@typescript-eslint/explicit-function-return-type': 'off',
+    },
+  },
 ];
